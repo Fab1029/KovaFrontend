18306246570e1068dd94956e75281745a7bb2baf
Fab1029 <andres.ledesma@ucuenca.edu.ec>
2025-08-12 11:37:11 -0500

    (feature): responsive who we are component

--- src/components/PhotosGallery/PhotosGallery.jsx
+++ src/components/PhotosGallery/PhotosGallery.jsx
@@ -22,7 +22,7 @@ const PhotosGallery = () => {
 
             <div className='body-right-container'>
                 <img data-aos = 'slide-left' data-aos-duration = '600' className='photo-element' src={photo3}/>
-                <img data-aos = 'slide-left' className='photo-element' src={photo4}/>
+                <img data-aos = 'slide-up' className='photo-element' src={photo4}/>
             </div>
             
         </div>
