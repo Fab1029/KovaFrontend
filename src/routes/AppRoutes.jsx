e8d48f9b3ef2fea3051653da09e7dc6ebbf81a59
Fab1029 <andres.ledesma@ucuenca.edu.ec>
2025-08-13 22:23:32 -0500

    3 Deploy

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from '../pages/Home/Home';
 import WhoWeAre from '../pages/WhoWeAre/WhoWeAre';
 import ProjectsGallery from '../pages/ProjectsGallery/ProjectsGallery';
